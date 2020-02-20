4affb324bf425816df109d18156ffc1301254904
PaulinaCz <paulinaczernieckaa@gmail.com>
2020-02-20 14:49:49 +0000

    Method implementation passed on various Tests

--- src/test/java/CheckingBracketsTest.java
+++ src/test/java/CheckingBracketsTest.java
@@ -32,4 +32,58 @@ public class CheckingBracketsTest {
 
     }
 
+    @Test
+    public void shouldReturnFalseWhenStringHasOpenBracketsOnly(){
+        //given
+        String str = " { ( {";
+
+        //when
+        boolean result = checkingBrackets.areBracketsClosing(str);
+
+        //then
+        assertFalse(result);
+    }
+
+    @Test
+    public void shouldReturnTrueIfBracketsClosedAroundOtherCharacters(){
+        //given
+        String str = "{To jest [kod] }";
+
+        //when
+        boolean result = checkingBrackets.areBracketsClosing(str);
+
+        assertTrue(result);
+    }
+
+    @Test
+    public void shouldReturnFalseIfBracketsOpenAroundOtherCharacters(){
+        //given
+        String str = "{To jest [kod }";
+
+        //when
+        boolean result = checkingBrackets.areBracketsClosing(str);
+
+        assertFalse(result);
+    }
+
+    @Test
+    public void shouldReturnTrueForClosedMixedBrackets(){
+        String str = "{Ten [kod bedzie juz (bardziej) skompikowany]}";
+
+        //when
+        boolean result = checkingBrackets.areBracketsClosing(str);
+
+        assertTrue(result);
+    }
+
+    @Test
+    public void shouldReturnFalseForWrongBracketsClosings(){
+        String str = "{Ten} [kod) bedzie juz {bardziej) skompikowany]}";
+
+        //when
+        boolean result = checkingBrackets.areBracketsClosing(str);
+
+        assertFalse(result);
+    }
+
 }
