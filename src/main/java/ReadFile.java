c6206a4fe4a7e3f5b7f49d4f9c61f3554417a40e
PaulinaCz <paulinaczernieckaa@gmail.com>
2020-02-20 12:49:44 +0000

    Added FileReader

---
+++ src/main/java/ReadFile.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ReadFile {
+
+    public String readFileOfCode(String file){
+
+        Path filePath = Paths.get("C:\\SDA\\homework1602\\code");
+        StringBuilder sb = new StringBuilder();
+        try
+        {
+            String stringToCheck = Files.readString(filePath);
+            sb.append(stringToCheck);
+
+        }
+
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        return sb.toString();
+    }
+}
---
+++ src/test/java/CheckingBracketsTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CheckingBracketsTest {
+
+    ReadFile file = new ReadFile();
+    String stringToCheck = file.readFileOfCode("C:\\SDA\\homework1602\\code");
+
+    @Test
+    public void checkingCodeWithBracketsOnly(){
+
+
+    }
+
+}
